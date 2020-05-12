6112cad66e427fd3e989260eeac43ea74ff1bd0a
jamiestephenkerr@gmail.com <jamiestephenkerr@gmail.com>
2020-05-12 11:28:58 +0100

    Add connection app.config and transform and configuration manager

---
+++ Blaise.Nuget.Api/Providers/ConfigurationProvider.cs
@@ -0,0 +1,35 @@
+﻿
+using Blaise.Nuget.Core.Models;
+using System;
+using System.Configuration;
+
+namespace Blaise.Nuget.Api.Providers
+{
+    public class ConfigurationProvider
+    {
+        public ConnectionModel GetConnectionModel()
+        {
+            var connectionModel = new ConnectionModel
+            {
+                ServerName = ConfigurationManager.AppSettings["BlaiseServerHostName"],
+                UserName = ConfigurationManager.AppSettings["BlaiseServerUserName"],
+                Password = ConfigurationManager.AppSettings["BlaiseServerPassword"],
+                Binding = ConfigurationManager.AppSettings["BlaiseServerBinding"],
+                Port = ConvertToInt(ConfigurationManager.AppSettings["BlaiseConnectionPort"]),
+            };
+            return connectionModel;
+        }
+
+        public ConnectionModel GetRemoteConnectionModel()
+        {
+            var connectionModel = GetConnectionModel();
+            connectionModel.Port = ConvertToInt(ConfigurationManager.AppSettings["BlaiseRemoteConnectionPort"]);
+            return connectionModel;
+        }
+
+        private int ConvertToInt(string integer)
+        {
+            return Int32.Parse(integer);
+        }
+    }
+}
