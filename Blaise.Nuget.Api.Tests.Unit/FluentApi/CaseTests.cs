42a5e5e9a56d027b59bd92f157bb9932906505d5
jamie kerr <jamiestephenkerr@gmail.com>
2020-06-02 13:01:07 +0100

    Updated to facilitate exists on case and serverpark

--- Blaise.Nuget.Api.Tests.Unit/FluentApi/CaseTests.cs
+++ Blaise.Nuget.Api.Tests.Unit/FluentApi/CaseTests.cs
@@ -410,5 +410,69 @@ namespace Blaise.Nuget.Api.Tests.Unit.FluentApi
             var exception = Assert.Throws<NullReferenceException>(() => _sut.MarkAsProcessed());
             Assert.AreEqual("The 'Case' step needs to be called prior to this to specify the data record of the case", exception.Message);
         }
+
+        [Test]
+        public void Given_Case_Is_Called_When_I_Call_Exists_Then_The_Correct_Service_Method_Is_Called()
+        {
+            //arrange
+
+            _blaiseApiMock.Setup(p => p.CaseExists(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(It.IsAny<bool>());
+
+            _sut.ServerPark(_serverParkName);
+            _sut.Instrument(_instrumentName);
+            _sut.Case(_primaryKeyValue);
+
+            //act
+            _sut.Exists();
+
+            //assert
+            _blaiseApiMock.Verify(v => v.CaseExists(_primaryKeyValue, _instrumentName, _serverParkName), Times.Once);
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void Given_Case_Is_Called_When_I_Call_Exists_Then_The_Expected_Result_Is_Returned(bool caseExists)
+        {
+            //arrange
+
+            _blaiseApiMock.Setup(p => p.CaseExists(_primaryKeyValue, _instrumentName, _serverParkName)).Returns(caseExists);
+
+            _sut.ServerPark(_serverParkName);
+            _sut.Instrument(_instrumentName);
+            _sut.Case(_primaryKeyValue);
+
+            //act
+            var result = _sut.Exists();
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(caseExists, result);
+        }
+
+        [Test]
+        public void Given_Case_Is_Called_But_Instrument_Has_Not_Been_Called_When_I_Call_Exists_Then_An_NullReferenceException_Is_Thrown()
+        {
+            //arrange
+
+            _sut.ServerPark(_serverParkName);
+            _sut.Case(_primaryKeyValue);
+
+            //act && assert
+            var exception = Assert.Throws<NullReferenceException>(() => _sut.Exists());
+            Assert.AreEqual("The 'Instrument' step needs to be called prior to this to specify the name of the instrument", exception.Message);
+        }
+
+        [Test]
+        public void Given_Case_Is_Called_But_ServerPark_Has_Not_Been_Called_When_I_Call_Exists_Then_An_NullReferenceException_Is_Thrown()
+        {
+            //arrange
+
+            _sut.Instrument(_instrumentName);
+            _sut.Case(_primaryKeyValue);
+
+            //act && assert
+            var exception = Assert.Throws<NullReferenceException>(() => _sut.Exists());
+            Assert.AreEqual("The 'ServerPark' step needs to be called prior to this to specify the name of the server park", exception.Message);
+        }
     }
 }
