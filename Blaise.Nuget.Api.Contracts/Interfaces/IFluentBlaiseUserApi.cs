4f913fd0213acf2fee2c413a1e10ac2a59cea753
jamie kerr <jamiestephenkerr@gmail.com>
2020-06-03 09:10:04 +0100

    Updated parameter to Ienumerable

--- Blaise.Nuget.Api.Contracts/Interfaces/IFluentBlaiseUserApi.cs
+++ Blaise.Nuget.Api.Contracts/Interfaces/IFluentBlaiseUserApi.cs
@@ -4,7 +4,7 @@ namespace Blaise.Nuget.Api.Contracts.Interfaces
 {
     public interface IFluentBlaiseUserApi
     {
-        void Add(string password, string role, IList<string> serverParkNames);
+        void Add(string password, string role, IEnumerable<string> serverParkNames);
 
         void ChangePassword(string password);
     }
