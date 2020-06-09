f3704b37cdcc6f362134ed092f24ed27e159d746
jamie kerr <jamiestephenkerr@gmail.com>
2020-06-09 18:52:41 +0100

    Updated to add integration test

--- Blaise.Nuget.Api.Tests.Unit/Integration/Handler/FluentApiToFileHandlerTests.cs
+++ Blaise.Nuget.Api.Tests.Unit/Integration/Handler/FluentApiToFileHandlerTests.cs
@@ -18,8 +18,30 @@ namespace Blaise.Nuget.Api.Tests.Unit.Integration.Handler
                 .WithServerPark("LocalDevelopment")
                 .WithInstrument("OPN2004A")
                 .Case
-                .WithPrimaryKey("1100021")
+                .WithPrimaryKey("11000141")
                 .Copy
+                .ToFile(@"d:\temp\OPN\Handler")
+                .ToInstrument("OPN2004A")
+                .Handle();
+
+            //assert
+        }
+
+        [Ignore("Wont run without app settings on build environment")]
+        [Test]
+        public void Given_Valid_Arguments_For_File_When_I_Call_Move_Then_A_Case_Is_Moved_To_A_File()
+        {
+            //arrange
+            IFluentBlaiseApi sut = new FluentBlaiseApi();
+
+            //act
+            sut
+                .WithServer("localhost")
+                .WithServerPark("LocalDevelopment")
+                .WithInstrument("OPN2004A")
+                .Case
+                .WithPrimaryKey("11000131")
+                .Move
                 .ToFile(@"d:\temp\OPN\Handler")
                 .ToInstrument("OPN2004A")
                 .Handle();
