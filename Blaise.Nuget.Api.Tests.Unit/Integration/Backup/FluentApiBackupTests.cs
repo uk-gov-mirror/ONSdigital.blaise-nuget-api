150a2e6c3b9168213d6763a9be29a2b6bb28fc7c
jamie kerr <jamiestephenkerr@gmail.com>
2020-07-01 10:09:30 +0100

    Added integration test

---
+++ Blaise.Nuget.Api.Tests.Unit/Integration/Backup/FluentApiBackupTests.cs
@@ -0,0 +1,38 @@
+﻿using Blaise.Nuget.Api.Contracts.Interfaces;
+using Blaise.Nuget.Api.Contracts.Models;
+using NUnit.Framework;
+
+namespace Blaise.Nuget.Api.Tests.Unit.Integration.Backup
+{
+    public class FluentApiBackupTests
+    {
+        [Ignore("Wont run without library directory being set in app settings")]
+        [Test]
+        public void Given_Valid_Arguments_When_I_Call_Backup_Then_A_Survey_Is_Backed_Up()
+        {
+            //arrange
+            IFluentBlaiseApi sut = new FluentBlaiseApi();
+
+            var connectionModel = new ConnectionModel
+            {
+                Binding = "HTTP",
+                UserName = "Root",
+                Password = "Root",
+                ServerName = "localhost",
+                Port = 8031,
+                RemotePort = 8033
+            };
+
+            //act
+            sut
+                .WithConnection(connectionModel)
+                .WithServerPark("LocalDevelopment")
+                .WithInstrument("OPN2004A")
+                .Survey
+                .ToPath(@"D:\Temp\OPN\Backup")
+                .Backup();
+
+            //assert
+        }
+    }
+}
