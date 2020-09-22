04a732b49e68d61f5453b89ff05dd4ab85122e38
jamie kerr <jamiestephenkerr@gmail.com>
2020-09-22 13:31:52 +0100

    Adeed NIsra enum to case status

--- Blaise.Nuget.Api.Contracts/Enums/CaseStatusType.cs
+++ Blaise.Nuget.Api.Contracts/Enums/CaseStatusType.cs
@@ -1,9 +1,14 @@
-﻿namespace Blaise.Nuget.Api.Contracts.Enums
+﻿using System.ComponentModel;
+
+namespace Blaise.Nuget.Api.Contracts.Enums
 {
     public enum CaseStatusType
     {
         NotSpecified = 0,
         Completed,
-        Processed
+        Processed,
+
+        [Description("NISRA record imported")]
+        NisraCaseImported
     }
 }
