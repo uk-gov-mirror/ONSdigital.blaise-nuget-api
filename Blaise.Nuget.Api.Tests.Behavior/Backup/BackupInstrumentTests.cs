eef3b263e2078e9ac778c6e79ed39963f15d6102
jamie kerr <jamiestephenkerr@gmail.com>
2020-11-10 15:40:40 +0000

    Added test project

--- Blaise.Nuget.Api.Tests.Behavior/Backup/BackupInstrumentTests.cs
+++ Blaise.Nuget.Api.Tests.Behavior/Backup/BackupInstrumentTests.cs
@@ -4,23 +4,23 @@ namespace Blaise.Nuget.Api.Tests.Behavior.Backup
 {
     public class BackupInstrumentTests
     {
-        private BlaiseApi sut;
+        private readonly BlaiseApi _sut;
 
         public BackupInstrumentTests()
         {
-            sut = new BlaiseApi();
+            _sut = new BlaiseApi();
         }
 
         [Test]
         public void Given_A_Valid_Instrument_When_I_Call_BackupToFile_Then_An_Instrument_Is_Backed_Up_To_File()
         {
             //arrange
-            var serverParkName = "Tel";
-            var instrumentName = "";
+            var serverParkName = "LocalDevelopment";
+            var instrumentName = "OPN2004A";
             var outputPath = @"d:\temp\backup";
 
             //act
-            sut.BackupSurveyToFile(sut.GetDefaultConnectionModel(), serverParkName, instrumentName, outputPath);
+            _sut.BackupSurveyToFile(_sut.GetDefaultConnectionModel(), serverParkName, instrumentName, outputPath);
 
             //arrange
         }
