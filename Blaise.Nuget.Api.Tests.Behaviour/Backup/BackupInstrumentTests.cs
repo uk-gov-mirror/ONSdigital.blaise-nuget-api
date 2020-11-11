32f088acff7179808d34fd8ca69f2e412d554308
Jamie Kerr <jamiestephenkerr@gmail.com>
2020-11-11 12:04:33 +0000

    Updated behaviour project to include statnet library

--- Blaise.Nuget.Api.Tests.Behaviour/Backup/BackupInstrumentTests.cs
+++ Blaise.Nuget.Api.Tests.Behaviour/Backup/BackupInstrumentTests.cs
@@ -11,13 +11,13 @@ namespace Blaise.Nuget.Api.Tests.Behaviour.Backup
             _sut = new BlaiseApi();
         }
 
-        //[Ignore("")]
+        [Ignore("")]
         [Test]
         public void Given_A_Valid_Instrument_When_I_Call_BackupToFile_Then_An_Instrument_Is_Backed_Up_To_File()
         {
             //arrange
             var serverParkName = "LocalDevelopment";
-            var instrumentName = "OPN2101A";
+            var instrumentName = "OPN2004A";
             var outputPath = @"d:\temp\backup";
 
             //act
