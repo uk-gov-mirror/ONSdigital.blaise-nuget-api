c13114a8e3c0fc33f454c65d94a12da8474a43ed
Jamie Kerr <jamiestephenkerr@gmail.com>
2020-12-09 09:05:40 +0000

    Updat methods to be more buiness focused

---
+++ Blaise.Nuget.Api.Tests.Behaviour/Case/CreateCaseTests.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Collections.Generic;
+using Blaise.Nuget.Api.Api;
+using Blaise.Nuget.Api.Contracts.Enums;
+using Blaise.Nuget.Api.Contracts.Extensions;
+using NUnit.Framework;
+
+namespace Blaise.Nuget.Api.Tests.Behaviour.Case
+{
+    public class CreateCaseTests
+    {
+        private readonly BlaiseCaseApi _sut;
+        private readonly string _primaryKey;
+
+        public CreateCaseTests()
+        {
+            _sut = new BlaiseCaseApi();
+            _primaryKey = "9000001";
+        }
+
+        [Ignore("Integration")]
+        [Test]
+        public void Given_Valid_Arguments_When_I_Call_CreateCase_Then_The_Case_Is_Created()
+        {
+            //arrange
+            var serverParkName = "LocalDevelopment";
+            var instrumentName = "OPN2101A";
+            var fieldData = new Dictionary<string, string>
+            {
+                {FieldNameType.HOut.FullName(), "110"},
+                {FieldNameType.TelNo.FullName(), "07000000000"},
+            };
+
+            //act
+            _sut.CreateCase(_primaryKey, fieldData, instrumentName, serverParkName);
+
+            //arrange
+            Assert.IsTrue(_sut.CaseExists(_primaryKey, instrumentName, serverParkName));
+
+            //cleanup
+            _sut.RemoveCase(_primaryKey, instrumentName, serverParkName);
+        }
+    }
+}
---
+++ Blaise.Nuget.Api.Tests.Behaviour/Case/GetCaseTests.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using Blaise.Nuget.Api.Api;
+using Blaise.Nuget.Api.Contracts.Enums;
+using Blaise.Nuget.Api.Contracts.Extensions;
+using NUnit.Framework;
+
+namespace Blaise.Nuget.Api.Tests.Behaviour.Case
+{
+    public class GetCaseTests
+    {
+        private readonly BlaiseCaseApi _sut;
+        private readonly string _primaryKey;
+
+        public GetCaseTests()
+        {
+            _sut = new BlaiseCaseApi();
+            _primaryKey = "9000001";
+        }
+
+        [Ignore("Integration")]
+        [Test]
+        public void Given_Valid_Arguments_When_I_Call_GetCase_Then_The_Case_is_Returned()
+        {
+            //arrange
+            var serverParkName = "LocalDevelopment";
+            var instrumentName = "OPN2101A";
+            var fieldData = new Dictionary<string, string>
+            {
+                {FieldNameType.HOut.FullName(), "110"},
+                {FieldNameType.TelNo.FullName(), "07000000000"},
+            };
+
+            _sut.CreateCase(_primaryKey, fieldData, instrumentName, serverParkName);
+
+            //act
+            var result = _sut.GetCase(_primaryKey,  instrumentName, serverParkName);
+
+            var fields = result.Fields.Select(f => f.FullName.Contains("tel"));
+            var fields2 = result.Fields.Select(f => f.FullName.Contains("Tel"));
+
+            //arrange
+            Assert.AreEqual(_primaryKey, _sut.GetPrimaryKeyValue(result));
+
+            //cleanup
+            _sut.RemoveCase(_primaryKey, instrumentName, serverParkName);
+        }
+    }
+}
