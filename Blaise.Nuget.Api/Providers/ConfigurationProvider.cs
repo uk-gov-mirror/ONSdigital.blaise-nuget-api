46d17e8f95c90947ce7dc5460f5183233a0aff98
Jamie Kerr <jamiestephenkerr@gmail.com>
2020-12-10 12:44:28 +0000

    Updated to throw error if config is missing

--- Blaise.Nuget.Api/Providers/ConfigurationProvider.cs
+++ Blaise.Nuget.Api/Providers/ConfigurationProvider.cs
@@ -2,6 +2,7 @@
 using System.Configuration;
 using Blaise.Nuget.Api.Contracts.Models;
 using Blaise.Nuget.Api.Core.Interfaces.Providers;
+using Blaise.Nuget.Api.Helpers;
 
 namespace Blaise.Nuget.Api.Providers
 {
@@ -11,23 +12,36 @@ namespace Blaise.Nuget.Api.Providers
         {
             var connectionModel = new ConnectionModel
             {
-                ServerName = Environment.GetEnvironmentVariable("ENV_BLAISE_SERVER_HOST_NAME", EnvironmentVariableTarget.Machine) ?? ConfigurationManager.AppSettings["BlaiseServerHostName"],
-                UserName = Environment.GetEnvironmentVariable("ENV_BLAISE_ADMIN_USER", EnvironmentVariableTarget.Machine) ?? ConfigurationManager.AppSettings["BlaiseServerUserName"],
-                Password = Environment.GetEnvironmentVariable("ENV_BLAISE_ADMIN_PASSWORD", EnvironmentVariableTarget.Machine) ?? ConfigurationManager.AppSettings["BlaiseServerPassword"],
-                Binding = Environment.GetEnvironmentVariable("ENV_BLAISE_SERVER_BINDING", EnvironmentVariableTarget.Machine) ?? ConfigurationManager.AppSettings["BlaiseServerBinding"],
-                Port = ConvertToInt(Environment.GetEnvironmentVariable("ENV_BLAISE_CONNECTION_PORT", EnvironmentVariableTarget.Machine) ?? ConfigurationManager.AppSettings["BlaiseConnectionPort"]),
-                RemotePort = ConvertToInt(Environment.GetEnvironmentVariable("ENV_BLAISE_REMOTE_CONNECTION_PORT", EnvironmentVariableTarget.Machine) ?? ConfigurationManager.AppSettings["BlaiseRemoteConnectionPort"]),
-                ConnectionExpiresInMinutes = ConvertToInt(Environment.GetEnvironmentVariable("ENV_CONNECTION_EXPIRES_IN_MINUTES", EnvironmentVariableTarget.Machine) ?? ConfigurationManager.AppSettings["ConnectionExpiresInMinutes"]),
+                ServerName = GetEnvironmentVariable("ENV_BLAISE_SERVER_HOST_NAME") ?? GetLocalVariable("BlaiseServerHostName"),
+                UserName = GetEnvironmentVariable("ENV_BLAISE_ADMIN_USER") ?? GetLocalVariable("BlaiseServerUserName"),
+                Password = GetEnvironmentVariable("ENV_BLAISE_ADMIN_PASSWORD") ?? GetLocalVariable("BlaiseServerPassword"),
+                Binding = GetEnvironmentVariable("ENV_BLAISE_SERVER_BINDING") ?? GetLocalVariable("BlaiseServerBinding"),
+                Port = ConvertToInt(GetEnvironmentVariable("ENV_BLAISE_CONNECTION_PORT") ?? GetLocalVariable("BlaiseConnectionPort")),
+                RemotePort = ConvertToInt(GetEnvironmentVariable("ENV_BLAISE_REMOTE_CONNECTION_PORT") ?? GetLocalVariable("BlaiseRemoteConnectionPort")),
+                ConnectionExpiresInMinutes = ConvertToInt(GetEnvironmentVariable("ENV_CONNECTION_EXPIRES_IN_MINUTES") ?? GetLocalVariable("ConnectionExpiresInMinutes"))
             };
             return connectionModel;
-        }         
+        }
 
-        public string LibraryDirectory => Environment.GetEnvironmentVariable("ENV_LIBRARY_DIRECTORY", EnvironmentVariableTarget.Machine) ?? ConfigurationManager.AppSettings["LibraryDirectory"];
+        public string LibraryDirectory => GetEnvironmentVariable("ENV_LIBRARY_DIRECTORY") ?? GetLocalVariable("LibraryDirectory");
 
         public int ConnectionExpiresInMinutes =>
             ConvertToInt(
-                Environment.GetEnvironmentVariable("ENV_CONNECTION_EXPIRES_IN_MINUTES",
-                    EnvironmentVariableTarget.Machine) ?? (string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["ConnectionExpiresInMinutes"]) ? "30" : ConfigurationManager.AppSettings["ConnectionExpiresInMinutes"]));
+                GetEnvironmentVariable("ENV_CONNECTION_EXPIRES_IN_MINUTES") ?? 
+                (string.IsNullOrWhiteSpace(GetLocalVariable("ConnectionExpiresInMinutes")) 
+                    ? "30" : GetLocalVariable("ConnectionExpiresInMinutes")));
+
+        private string GetEnvironmentVariable(string variableName)
+        {
+            return Environment.GetEnvironmentVariable(variableName, EnvironmentVariableTarget.Machine);
+        }
+
+        private string GetLocalVariable(string variableName)
+        {
+            var variable = ConfigurationManager.AppSettings[variableName];
+            variable.ThrowExceptionIfNullOrEmpty(variableName);
+            return variable;
+        }
 
         private static int ConvertToInt(string integer)
         {
