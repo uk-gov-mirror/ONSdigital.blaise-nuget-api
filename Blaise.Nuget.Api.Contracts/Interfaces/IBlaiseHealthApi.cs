e721552bf703ef899f490c18b496299d8cdd943b
jamie kerr <jamiestephenkerr@gmail.com>
2020-12-17 09:33:14 +0000

    Added interface for health api

---
+++ Blaise.Nuget.Api.Contracts/Interfaces/IBlaiseHealthApi.cs
@@ -0,0 +1,10 @@
+﻿namespace Blaise.Nuget.Api.Contracts.Interfaces
+{
+    public interface IBlaiseHealthApi
+    {
+        bool ConnectionModelIsHealthy();
+        bool ConnectionToBlaiseIsHealthy();
+        bool RemoteConnectionToBlaiseIsHealthy();
+        bool RemoteConnectionToCatiIsHealthy();
+    }
+}
