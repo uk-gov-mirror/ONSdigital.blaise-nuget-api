931d7bb2884e46a3f47bf216d5af8055c667d95a
Jamie Kerr <jamiestephenkerr@gmail.com>
2021-01-08 11:54:39 +0000

    Updated datainterface

--- Blaise.Nuget.Api.Core/Services/DataInterfaceService.cs
+++ Blaise.Nuget.Api.Core/Services/DataInterfaceService.cs
@@ -3,6 +3,7 @@ using Blaise.Nuget.Api.Contracts.Models;
 using Blaise.Nuget.Api.Core.Interfaces.Factories;
 using Blaise.Nuget.Api.Core.Interfaces.Providers;
 using Blaise.Nuget.Api.Core.Interfaces.Services;
+using StatNeth.Blaise.API.DataInterface;
 
 namespace Blaise.Nuget.Api.Core.Services
 {
@@ -21,10 +22,19 @@ namespace Blaise.Nuget.Api.Core.Services
 
         public void UpdateDataInterfaceConnection(ConnectionModel connectionModel, Guid instrumentGuid)
         {
-            var dataInterface = _dataInterfaceFactory.GetConnection(connectionModel, instrumentGuid);
+            //var dataInterface = _dataInterfaceFactory.GetConnection(connectionModel, instrumentGuid);
             var databaseConnectionString = _configurationProvider.DatabaseConnectionString;
+            
+            var dataInterface = DataInterfaceManager.GetDataInterface();
+            dataInterface.ConnectionInfo.DataSourceType = DataSourceType.Blaise;
+            dataInterface.ConnectionInfo.DataProviderType =DataProviderType.BlaiseDataProviderForDotNET;
+            dataInterface.DataPartitionType = DataPartitionType.Stream;
 
-            dataInterface.ConnectionInfo.SetConnectionString(databaseConnectionString);
+            var connectionStringBuilder = DataInterfaceManager.GetBlaiseConnectionStringBuilder();
+            connectionStringBuilder.DataSource = databaseConnectionString; //@"D:\Flight.bdbx";
+            dataInterface.ConnectionInfo.SetConnectionString(connectionStringBuilder.ConnectionString);
+            dataInterface.DatamodelFileName = @"D:\Flight.bmix";
+
             dataInterface.CreateTableDefinitions();
             dataInterface.CreateDatabaseObjects(databaseConnectionString, true);
             dataInterface.SaveToFile(true);
