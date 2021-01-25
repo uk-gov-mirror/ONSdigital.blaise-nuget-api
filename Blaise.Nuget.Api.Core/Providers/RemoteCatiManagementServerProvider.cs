cf3fd73bed0745cc5c265c42980239c6a29fb387
jamie kerr <jamiestephenkerr@gmail.com>
2021-01-25 12:48:14 +0000

    Updated cati api to return installed surveys

---
+++ Blaise.Nuget.Api.Core/Providers/RemoteCatiManagementServerProvider.cs
@@ -0,0 +1,26 @@
+﻿using Blaise.Nuget.Api.Contracts.Models;
+using Blaise.Nuget.Api.Core.Interfaces.Factories;
+using Blaise.Nuget.Api.Core.Interfaces.Providers;
+using StatNeth.Blaise.API.Cati.Runtime;
+
+namespace Blaise.Nuget.Api.Core.Providers
+{
+    public class RemoteCatiManagementServerProvider : IRemoteCatiManagementServerProvider
+    {
+        private readonly ICatiManagementServerFactory _catiServerFactory;
+
+        public RemoteCatiManagementServerProvider(ICatiManagementServerFactory catiServerFactory)
+        {
+            _catiServerFactory = catiServerFactory;
+        }
+
+        public IRemoteCatiManagementServer GetCatiManagementForServerPark(ConnectionModel connectionModel, string serverParkName)
+        {
+            var catiManagement = _catiServerFactory.GetConnection(connectionModel);
+
+            catiManagement.SelectServerPark(serverParkName);
+
+            return catiManagement;
+        }
+    }
+}
