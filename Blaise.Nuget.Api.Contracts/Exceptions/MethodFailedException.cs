a64dffee3cf17f4781de6d758954f806bf031f19
Jamie Kerr <jamiestephenkerr@gmail.com>
2021-03-04 08:33:49 +0000

    added new exception

---
+++ Blaise.Nuget.Api.Contracts/Exceptions/MethodFailedException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Blaise.Nuget.Api.Contracts.Exceptions
+{
+    public class MethodFailedException : Exception
+    {
+        public MethodFailedException(string message) : base(message)
+        {
+        }
+    }
+}
