046a2661d30474e0ea7590f7752ae1a3cd5f67f4
elthorne <elinor.thorne@ons.gov.uk>
2023-06-19 15:12:04 +0100

    WIP...

---
+++ Blaise.Nuget.Api/Api/BlaiseAuditTrailApi.cs
@@ -0,0 +1,44 @@
+﻿using Blaise.Nuget.Api.Contracts.Interfaces;
+using Blaise.Nuget.Api.Contracts.Models;
+using Blaise.Nuget.Api.Core.Interfaces.Providers;
+using Blaise.Nuget.Api.Core.Interfaces.Services;
+using Blaise.Nuget.Api.Extensions;
+using Blaise.Nuget.Api.Providers;
+using StatNeth.Blaise.API.DataInterface;
+using System;
+using System.Security;
+using ATA = StatNeth.Blaise.API.AuditTrail;
+
+
+namespace Blaise.Nuget.Api.Api
+{
+    public class BlaiseAuditTrailApi : IBlaiseAuditTrailApi
+    {
+        public void GetAuditTrail()
+        {
+            var configurationProvider = UnityProvider.Resolve<IBlaiseConfigurationProvider>();
+            var password = GetPassword(configurationProvider.GetConnectionModel.Password);
+            var ras =
+               ATA.AuditTrailManager.GetRemoteAuditTrailServer(
+                   configurationProvider.GetConnectionModel.ServerName,
+                   configurationProvider.GetConnectionModel.Port,
+                   configurationProvider.GetConnectionModel.Username,
+                   password);
+        }
+
+        private static SecureString GetPassword(string pw)
+        {
+            char[] passwordChars = pw.ToCharArray();
+            var password = new SecureString();
+
+            foreach (var c in passwordChars)
+            {
+                password.AppendChar(c);
+            }
+            return password;
+        }
+
+
+    }
+}
+
