3beaaff15e0c80a892e36a223298eca7b69a2e76
alistair-platt <alistair.platt@ons.gov.uk>
2023-06-19 15:20:46 +0100

    Tweaks

--- Blaise.Nuget.Api/Api/BlaiseAuditTrailApi.cs
+++ Blaise.Nuget.Api/Api/BlaiseAuditTrailApi.cs
@@ -1,11 +1,7 @@
 ﻿using Blaise.Nuget.Api.Contracts.Interfaces;
 using Blaise.Nuget.Api.Contracts.Models;
 using Blaise.Nuget.Api.Core.Interfaces.Providers;
-using Blaise.Nuget.Api.Core.Interfaces.Services;
-using Blaise.Nuget.Api.Extensions;
 using Blaise.Nuget.Api.Providers;
-using StatNeth.Blaise.API.DataInterface;
-using System;
 using System.Security;
 using ATA = StatNeth.Blaise.API.AuditTrail;
 
@@ -14,15 +10,27 @@ namespace Blaise.Nuget.Api.Api
 {
     public class BlaiseAuditTrailApi : IBlaiseAuditTrailApi
     {
+        private readonly ConnectionModel _connectionModel;
+
+        public BlaiseAuditTrailApi(ConnectionModel connection)
+        {
+            _connectionModel = connection;
+        }
+
         public void GetAuditTrail()
         {
+            if (_connectionModel is null)
+            {
+
+            }
+
             var configurationProvider = UnityProvider.Resolve<IBlaiseConfigurationProvider>();
-            var password = GetPassword(configurationProvider.GetConnectionModel.Password);
+            var password = GetPassword(configurationProvider.GetConnectionModel().Password);
             var ras =
                ATA.AuditTrailManager.GetRemoteAuditTrailServer(
-                   configurationProvider.GetConnectionModel.ServerName,
-                   configurationProvider.GetConnectionModel.Port,
-                   configurationProvider.GetConnectionModel.Username,
+                   configurationProvider.GetConnectionModel().ServerName,
+                   configurationProvider.GetConnectionModel().Port,
+                   configurationProvider.GetConnectionModel().Username,
                    password);
         }
 
