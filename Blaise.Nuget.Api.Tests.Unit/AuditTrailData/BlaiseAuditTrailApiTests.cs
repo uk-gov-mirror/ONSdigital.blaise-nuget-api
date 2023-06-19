4e3fbb54c3edbf783953995571322829822a6d5d
alistair-platt <alistair.platt@ons.gov.uk>
2023-06-19 16:04:07 +0100

    AutitTrail test

---
+++ Blaise.Nuget.Api.Tests.Unit/AuditTrailData/BlaiseAuditTrailApiTests.cs
@@ -0,0 +1,25 @@
+﻿using Blaise.Nuget.Api.Api;
+using Blaise.Nuget.Api.Contracts.Models;
+using NUnit.Framework;
+
+namespace Blaise.Nuget.Api.Tests.Unit.AuditTrailData
+{
+    public class BlaiseAuditTrailApiTests
+    {
+        private readonly ConnectionModel _connectionModel;
+
+        public BlaiseAuditTrailApiTests()
+        {
+            _connectionModel = new ConnectionModel();
+        }
+
+        [Test]
+        public void Given_No_ConnectionModel_When_I_Instantiate_BlaiseFileApi_No_Exceptions_Are_Thrown()
+        {
+            //act && assert
+            // ReSharper disable once ObjectCreationAsStatement
+            Assert.DoesNotThrow(() => new BlaiseFileApi());
+        }
+    }
+}
+
