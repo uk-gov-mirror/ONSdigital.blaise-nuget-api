28f6ba9e4688af2c69c0a835efb1cad9646d2c43
elthorne <elinor.thorne@ons.gov.uk>
2023-06-19 15:50:08 +0100

    wip...help me...

---
+++ Blaise.Nuget.Api.Tests.Unit/Api/BlaiseAuditTrail/BlaiseAuditTrailApiTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class BlaiseAuditTrailTest
+{
+    public BlaiseAuditTrailTest()
+    {
+        private readonly ConnectionModel _connectionModel;
+
+    public BlaiseAuditTrailTest()
+    {
+        _connectionModel = new ConnectionModel();
+    }
+
+    [Test]
+    public void Given_No_ConnectionModel_When_I_Instantiate_BlaiseFileApi_No_Exceptions_Are_Thrown()
+    {
+        //act && assert
+        // ReSharper disable once ObjectCreationAsStatement
+        Assert.DoesNotThrow(() => new BlaiseFileApi());
+    }
+}
+}
