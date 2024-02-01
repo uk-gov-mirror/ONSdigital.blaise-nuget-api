03265f4c9bd453a91be23759572760fd4ecb632a
alistair-platt <alistair.platt@ons.gov.uk>
2024-02-01 14:15:03 +0000

    Initial changes for the drop table commands

---
+++ Blaise.Nuget.Api.Tests.Behaviour/Sql/DropDatabaseTablesTests.cs
@@ -0,0 +1,32 @@
+﻿using Blaise.Nuget.Api.Api;
+using NUnit.Framework;
+
+namespace Blaise.Nuget.Api.Tests.Behaviour.Sql
+{
+    public class DropDatabaseTablesTests
+    {
+        private readonly BlaiseSqlApi _sut;
+
+        public DropDatabaseTablesTests()
+        {
+            _sut = new BlaiseSqlApi();
+        }
+
+        [Ignore("Integration")]
+        [Test]
+        public void Given_An_QuestionnaireName_And_PrimaryKey_When_I_Call_GetPostCode_I_Get_A_PostCode_Back()
+        {
+            //arrange
+            /* const string questionnaireName = "dst2106A";
+
+             //act
+             var result = _sut.
+
+             //assert
+             Assert.IsNotNull(result);
+             Assert.AreEqual("NP899XX", result);*/
+        }
+
+
+    }
+}
--- Blaise.Nuget.Api.Tests.Behaviour/Sql/PostCodeTests.cs
+++ Blaise.Nuget.Api.Tests.Behaviour/Sql/PostCodeTests.cs
@@ -6,13 +6,13 @@ namespace Blaise.Nuget.Api.Tests.Behaviour.Sql
     public class PostCodeTests
     {
         private readonly BlaiseSqlApi _sut;
-        
+
         public PostCodeTests()
         {
             _sut = new BlaiseSqlApi();
         }
 
-        [Ignore("Integration")]
+        //[Ignore("Integration")]
         [Test]
         public void Given_An_QuestionnaireName_And_PrimaryKey_When_I_Call_GetPostCode_I_Get_A_PostCode_Back()
         {
