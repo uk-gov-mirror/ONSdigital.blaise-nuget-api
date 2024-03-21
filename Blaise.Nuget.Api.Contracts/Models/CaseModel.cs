f896524449cccc4706d1c87118d16c436f3ab09f
jamie kerr <jamiestephenkerr@gmail.com>
2024-03-21 09:49:29 +0000

    Added method and tests to get primarykeyvalue from models

--- Blaise.Nuget.Api.Contracts/Models/CaseModel.cs
+++ Blaise.Nuget.Api.Contracts/Models/CaseModel.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Blaise.Nuget.Api.Contracts.Models
 {
     public class CaseModel
     {
+        public CaseModel()
+        {
+            PrimaryKeyValues = new Dictionary<string, string>();
+        }
+
         public CaseModel(Dictionary<string, string> primaryKeyValues, Dictionary<string, string> fieldData)
         {
             PrimaryKeyValues = primaryKeyValues;
@@ -13,5 +19,15 @@ namespace Blaise.Nuget.Api.Contracts.Models
         public Dictionary<string, string> PrimaryKeyValues { get; set; }
 
         public Dictionary<string, string> FieldData { get; set; }
+
+        public string GetPrimaryKeyValue(string primaryKeyName)
+        {
+            if (PrimaryKeyValues == null || PrimaryKeyValues.Count == 0)
+            {
+                throw new ArgumentOutOfRangeException("primaryKeyName", "There are no primary keys defined");
+            }
+
+            return PrimaryKeyValues[primaryKeyName];
+        }
     }
 }
