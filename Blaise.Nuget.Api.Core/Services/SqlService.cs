6a31eaf4fc989ef0856a8e45744892c167d0fd14
elthorne <elinor.thorne@ons.gov.uk>
2024-03-25 16:11:53 +0000

    refactor: remove unused imports
    refs: BLAIS5-4123

--- Blaise.Nuget.Api.Core/Services/SqlService.cs
+++ Blaise.Nuget.Api.Core/Services/SqlService.cs
@@ -3,9 +3,7 @@ using Blaise.Nuget.Api.Contracts.Extensions;
 using Blaise.Nuget.Api.Contracts.Models;
 using Blaise.Nuget.Api.Core.Interfaces.Services;
 using MySql.Data.MySqlClient;
-using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 
 namespace Blaise.Nuget.Api.Core.Services
 {
