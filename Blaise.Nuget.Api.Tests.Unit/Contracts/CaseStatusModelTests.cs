8a8e5160a67e07ba09b2f616d7ceba846d0a94e6
jamie kerr <jamiestephenkerr@gmail.com>
2024-03-27 12:01:43 +0000

    Added some additional unit tests

--- Blaise.Nuget.Api.Tests.Unit/Contracts/CaseStatusModelTests.cs
+++ Blaise.Nuget.Api.Tests.Unit/Contracts/CaseStatusModelTests.cs
@@ -26,6 +26,23 @@ namespace Blaise.Nuget.Api.Tests.Unit.Contracts
         }
 
         [Test]
+        public void Given_A_CaseStatusModel_Has_Populated_PrimaryKeys_When_I_Call_PrimaryKey_The_Correct_PrimaryKey_Value_Is_Returned()
+        {
+            // arrange
+            var primaryKeyName = "QID.Serial_Number";
+            var primaryKeyValue = "900001";
+            var primaryKeyValues = new Dictionary<string, string> { { primaryKeyName, primaryKeyValue } };
+            var caseStatusModel = new CaseStatusModel(primaryKeyValues, 110, DateTime.Now.ToString(CultureInfo.InvariantCulture));
+
+            // act
+            var result = caseStatusModel.PrimaryKey;
+
+            // assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(primaryKeyValue, result);
+        }
+
+        [Test]
         public void Given_A_CaseStatusModel_Does_Not_Have_Any_PrimaryKeys_When_I_Call_GetPrimaryKeyValue_Then_An_ArgumentOutOfRangeException_Is_Thrown()
         {
             // arrange
@@ -34,7 +51,20 @@ namespace Blaise.Nuget.Api.Tests.Unit.Contracts
 
             //act && assert
             var exception = Assert.Throws<ArgumentOutOfRangeException>(() => caseStatusModel.GetPrimaryKeyValue(primaryKeyName));
-            Assert.AreEqual("There are no primary keys defined\r\nParameter name: primaryKeyName", exception?.Message); ;
+            Assert.AreEqual("There are no primary keys defined\r\nParameter name: primaryKeyName", exception?.Message); 
+        }
+
+        [Test]
+        public void Given_A_CaseStatusModel_Does_Not_Have_The_Expected_PrimaryKeys_When_I_Call_GetPrimaryKeyValue_Then_An_ArgumentException_Is_Thrown()
+        {
+            // arrange
+            var primaryKeyName = "QID.Serial_Number";
+            var primaryKeyValues = new Dictionary<string, string> { { "MainSurveyID", "dgss-5ghghg-ttggh" } };
+            var caseStatusModel = new CaseStatusModel(primaryKeyValues, 110, DateTime.Now.ToString(CultureInfo.InvariantCulture));
+
+            //act && assert
+            var exception = Assert.Throws<ArgumentException>(() => caseStatusModel.GetPrimaryKeyValue(primaryKeyName));
+            Assert.AreEqual($"The primary key name '{primaryKeyName}' does not exist in the primaryKey object", exception?.Message);
         }
 
         [Test]
