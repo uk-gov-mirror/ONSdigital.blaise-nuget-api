b7f27cd1bccae53b2e5cd9cf8f4c8e338cdc1e83
jamie kerr <jamiestephenkerr@gmail.com>
2024-09-20 12:32:49 +0100

    Revert nuget to use 5.9.7 version of the statneth dll

--- Blaise.Nuget.Api.Contracts/Models/InstallOptions.cs
+++ Blaise.Nuget.Api.Contracts/Models/InstallOptions.cs
@@ -20,6 +20,5 @@ namespace Blaise.Nuget.Api.Contracts.Models
         public bool UploadSessionData { get; set; }
         public bool AllowDownloadOverMeteredConnection { get; set; }
         public string InitialAppLayoutSetGroupName { get; set; }
-        public DataConversionMode DataConversionMode { get; set; }
     }
 }
