78df8de225a2a0f0b0282d88082a16f492b1ec7c
Kristian T. <57638182+kristian4res@users.noreply.github.com>
2025-12-19 13:30:25 +0000

    fix: missing db connection

--- Blaise.Nuget.Api.Core/Services/SqlService.cs
+++ Blaise.Nuget.Api.Core/Services/SqlService.cs
@@ -104,6 +104,7 @@ namespace Blaise.Nuget.Api.Core.Services
             using (var cmd = new MySqlCommand())
             {
                 con.Open();
+                cmd.Connection = con;
                 cmd.CommandText = $"SELECT {SqlFieldType.PostCode.FullName()} from `{databaseTableName}` WHERE {SqlFieldType.CaseId.FullName()} = @primaryKey";
                 cmd.Parameters.AddWithValue("@primaryKey", primaryKey);
 
